ec7d02f4e2f0cbb75647b35df1f8c695a7e2210e
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2018-02-22 15:53:47 +0000

    Add eth_syncing method that returns false
    Signed-off-by: Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>

--- families/seth/rpc/src/calls/transaction.rs
+++ families/seth/rpc/src/calls/transaction.rs
@@ -53,6 +53,7 @@ pub fn get_method_list<T>() -> Vec<(String, RequestHandler<T>)> where T: Message
     methods.push((String::from("eth_estimateGas"), estimate_gas));
     methods.push((String::from("eth_sign"), sign));
     methods.push((String::from("eth_call"), call));
+    methods.push((String::from("eth_syncing"), syncing));
 
     methods
 }
@@ -345,4 +346,10 @@ pub fn call<T>(_params: Params, mut _client: ValidatorClient<T>) -> Result<Value
     info!("eth_call");
     // Implementing this requires running the EVM, which is not possible within the RPC.
     Err(error::not_implemented())
+}
+
+// Always return false
+pub fn syncing<T>(_params: Params, mut _client: ValidatorClient<T>) -> Result<Value, Error> where T: MessageSender {
+    info!("eth_syncing");
+    Ok(Value::Bool(false))
 }
