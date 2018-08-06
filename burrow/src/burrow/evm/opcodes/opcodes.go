7c7af3562a1a9245f17a32ec354eba384d08c7d0
Kenneth Koski <knkski@bitwise.io>
2018-08-06 10:32:46 -0500

    Remove dead code
    This function is not in use, and is causing the builds to fail, so
    removing it
    
    Signed-off-by: Kenneth Koski <knkski@bitwise.io>

--- burrow/src/burrow/evm/opcodes/opcodes.go
+++ burrow/src/burrow/evm/opcodes/opcodes.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 
 	"burrow/word256"
-	"gopkg.in/fatih/set.v0"
 )
 
 type OpCode byte
@@ -353,23 +352,6 @@ func (o OpCode) String() string {
 }
 
 //-----------------------------------------------------------------------------
-
-func AnalyzeJumpDests(code []byte) (dests *set.Set) {
-	dests = set.New()
-
-	for pc := uint64(0); pc < uint64(len(code)); pc++ {
-		var op OpCode = OpCode(code[pc])
-		switch op {
-		case PUSH1, PUSH2, PUSH3, PUSH4, PUSH5, PUSH6, PUSH7, PUSH8, PUSH9, PUSH10, PUSH11, PUSH12, PUSH13, PUSH14, PUSH15, PUSH16, PUSH17, PUSH18, PUSH19, PUSH20, PUSH21, PUSH22, PUSH23, PUSH24, PUSH25, PUSH26, PUSH27, PUSH28, PUSH29, PUSH30, PUSH31, PUSH32:
-			a := uint64(op) - uint64(PUSH1) + 1
-
-			pc += a
-		case JUMPDEST:
-			dests.Add(pc)
-		}
-	}
-	return
-}
 
 // Convenience function to allow us to mix bytes, ints, and OpCodes that
 // represent bytes in an EVM assembly code to make assembly more readable.
