72253baba6f08559653264162ccd2fb88bed618b
Rob Hitchens <26111999+rob-Hitchens@users.noreply.github.com>
2018-11-28 11:00:36 -0700

    Update hello_world_caller.sol
    refactor for solc 0.5.0

--- contracts/examples/contract_chaining/hello_world_caller.sol
+++ contracts/examples/contract_chaining/hello_world_caller.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.0;
+pragma solidity 0.5.0;
 
 contract HelloWorld {
-	function helloWorld() returns(bytes32);
+	function helloWorld() public returns(bytes32);
 }
 contract HelloWorldCaller {
-	function callHelloWorld(address helloWorldAddr) returns(bytes32) {
+	function callHelloWorld(address helloWorldAddr) public returns(bytes32) {
 		HelloWorld hello = HelloWorld(helloWorldAddr);
 		return hello.helloWorld();
 	}
