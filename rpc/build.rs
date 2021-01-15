23a93077000d317949dfab24796e8a53223c4977
Ryan Beck-Buysse <rbuysse@bitwise.io>
2021-01-14 19:02:06 -0600

    Update copyright headers to include 2021
    Signed-off-by: Ryan Beck-Buysse <rbuysse@bitwise.io>

--- rpc/build.rs
+++ rpc/build.rs
@@ -1,6 +1,6 @@
 /*
  * Copyright 2017 Intel Corporation
- * Copyright 2018-2020 Cargill Incorporated
+ * Copyright 2018-2021 Cargill Incorporated
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
